f205d3d9ef4a552b177c5f400437c527d815fbfb
bcomtest <amslibref@gmail.com>
2026-07-20 15:12:27 +0700

    Change view reason button in AdminClassRequests to i icon

--- frontend/src/pages/AdminClassRequests.jsx
+++ frontend/src/pages/AdminClassRequests.jsx
@@ -513,21 +513,11 @@ const AdminClassRequests = () => {
                                   e.stopPropagation();
                                   handleViewReason(request.admin_notes || request.admin_comment)
                                 }}
-                                className="text-blue-500 hover:text-blue-700 p-1 rounded-full transition-colors"
+                                className="text-red-500 hover:text-red-700 p-1 rounded-full transition-colors"
                                 title="ดูเหตุผลที่ไม่สามารถดำเนินการได้"
                               >
-                                <svg
-                                  xmlns="http://www.w3.org/2000/svg"
-                                  className="h-5 w-5"
-                                  viewBox="0 0 20 20"
-                                  fill="currentColor"
-                                >
-                                  <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
-                                  <path
-                                    fillRule="evenodd"
-                                    d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.022 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z"
-                                    clipRule="evenodd"
-                                  />
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                  <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
                                 </svg>
                               </button>
                             )}
@@ -625,21 +615,11 @@ const AdminClassRequests = () => {
                               e.stopPropagation();
                               handleViewReason(request.admin_notes || request.admin_comment)
                             }}
-                            className="text-blue-500 hover:text-blue-700 p-1 rounded-full transition-colors"
+                            className="text-red-500 hover:text-red-700 p-1 rounded-full transition-colors"
                             title="ดูเหตุผลที่ไม่สามารถดำเนินการได้"
                           >
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              className="h-5 w-5"
-                              viewBox="0 0 20 20"
-                              fill="currentColor"
-                            >
-                              <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
-                              <path
-                                fillRule="evenodd"
-                                d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.022 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z"
-                                clipRule="evenodd"
-                              />
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                              <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
                             </svg>
                           </button>
                         )}
